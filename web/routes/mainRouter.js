643f8100187afd5555b2d3d835ffc63f12a734fb
ash <ansh0827@naver.com>
2024-07-27 15:47:08 +0900

    사이드바 추가

--- web/routes/mainRouter.js
+++ web/routes/mainRouter.js
@@ -21,6 +21,16 @@ router.get('/plusPost', (req, res) => {
     res.render('plusPost',  { user: req.session.user })
 })
 
+// 수정 페이지로 이동
+router.get('/changePost', (req,res)=> {
+    res.render('changePost', {user: req.session.user})
+})
+
+// 자랑게시판 List로 이동
+router.get('/bragList', (req, res)=>{
+    res.render('bragList', {user: req.session.user})
+})
+
 // 다이어리 페이지 이동
 router.get('/diary', (req, res) => {
     res.render('diary', {user: req.session.user})
@@ -31,25 +41,15 @@ router.get('/dictionary', (req, res)=>{
     res.render('dictionary', {user: req.session.user})
 })
 
-// 자랑게시판 List로 이동
-router.get('/bragList', (req, res)=>{
-    res.render('bragList', {user: req.session.user})
+// 사전 세부 페이지로 이동
+router.get('/dictDetail', (req, res)=>{
+    res.render('dictDetail', {user: req.session.user})
 })
-
 
 // 병충해 진단 페이지
 router.get('/predict', (req, res) => {
     res.render('predict', {user: req.session.user})
 })
 
-// 사전 세부 페이지로 이동
-router.get('/dictDetail', (req, res)=>{
-    res.render('dictDetail', {user: req.session.user})
-})
-
-// 수정 페이지로 이동
-router.get('/changePost', (req,res)=> {
-    res.render('changePost', {user: req.session.user})
-})
 
 module.exports = router
